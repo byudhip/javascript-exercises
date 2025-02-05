15ee8f60fea3dc4a546d4aac093641d3ed3cdd20
Yudhi <baskyudh@gmail.com>
2025-02-05 09:34:55 +0700

    Completed all tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  if (num === 0) return "";
+  if (num < 0) return "ERROR";
+  if (!str) return "";
+  let append = str;
+  for (let i = 1; i < num; i++) {
+    str += append;
+  }
+  return str;
 };
 
 // Do not edit below this line
