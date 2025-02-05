36a910d80f2027a43699c8fcb362b3c5b7795dc0
Yudhi <baskyudh@gmail.com>
2025-02-05 11:14:46 +0700

    Completed all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanedStr = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
+    const revStr = cleanedStr.split("").reverse().join("");
+    return cleanedStr === revStr;
 };
 
 // Do not edit below this line
