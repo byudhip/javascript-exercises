85f5e6a75262d5905bf48d45367f6a749fc7c841
Yudhi <baskyudh@gmail.com>
2025-02-05 10:03:49 +0700

    Completed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, rmv1, rmv2, rmv3, rmv4) {
+    const toRemove = [rmv1, rmv2, rmv3, rmv4]
+    return arr.filter(item => !toRemove.includes(item));
 };
 
 // Do not edit below this line
