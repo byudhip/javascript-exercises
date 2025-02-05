c3a863ecee8304ddeb95804a2c79caea96b0b030
Yudhi <baskyudh@gmail.com>
2025-02-05 10:48:29 +0700

    Completed all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let converted = (f - 32) * (5/9);
+  return converted === 0 ? converted : Number(converted.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let converted = c * (9/5) + 32;
+  return converted === 0 ? converted : Number(converted.toFixed(1));
 };
 
 // Do not edit below this line
