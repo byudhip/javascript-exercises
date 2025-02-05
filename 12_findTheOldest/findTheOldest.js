e53392172e9a6377402e5e49843b1525d15d05c4
Yudhi <baskyudh@gmail.com>
2025-02-05 12:51:28 +0700

    Completed all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    return people.reduce((oldest, person) => {
+        let currentYear = new Date().getFullYear();
+        let personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+        let oldestAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
 
+        return personAge > oldestAge ? person : oldest;
+    }, people[0]);
 };
 
 // Do not edit below this line
