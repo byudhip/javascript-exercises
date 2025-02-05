92229e295d5e00f6de55d3e32b96203279829c87
Yudhi <baskyudh@gmail.com>
2025-02-05 11:15:36 +0700

    Refined variable names

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,7 +1,7 @@
 const palindromes = function (str) {
     const cleanedStr = str.replace(/[^a-z0-9]/gi, '').toLowerCase();
-    const revStr = cleanedStr.split("").reverse().join("");
-    return cleanedStr === revStr;
+    const reversedStr = cleanedStr.split("").reverse().join("");
+    return cleanedStr === reversedStr;
 };
 
 // Do not edit below this line
