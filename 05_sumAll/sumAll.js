fe0b9444bf4ceb0be6db2ffde0af9f084e202132
Yudhi <baskyudh@gmail.com>
2025-02-05 10:24:28 +0700

    Completed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  if (
+    typeof num1 !== "number" ||
+    typeof num2 !== "number" ||
+    num1 < 0 ||
+    num2 < 0 ||
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num1) 
+  )
+    return "ERROR";
+  let n = num1 > num2 ? num1 : num2;
+  return n*(n+1)/2;
 };
 
 // Do not edit below this line
