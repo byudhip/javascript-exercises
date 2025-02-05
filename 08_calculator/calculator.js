6fc00942b402e6e4db51978df1e52f04453fa1de
Yudhi <baskyudh@gmail.com>
2025-02-05 11:04:33 +0700

    Reformatted the codes

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,26 @@
-const add = function(num1, num2) {
-	return num1 + num2;
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function(num1, num2) {
-	return num1 - num2;
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function(arr) {
-	return arr.reduce((a,b) => a + b, 0);
+const sum = function (arr) {
+  return arr.reduce((a, b) => a + b, 0);
 };
 
-const multiply = function(arr) {
-  return arr.reduce((a,b) => a * b, 1);
+const multiply = function (arr) {
+  return arr.reduce((a, b) => a * b, 1);
 };
 
-const power = function(num1, num2) {
-	return num1 ** num2;
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function(num) {
-	if (num === 0) return 1;
-  for(let i = num-1; i > 0; i--) {
+const factorial = function (num) {
+  if (num === 0) return 1;
+  for (let i = num - 1; i > 0; i--) {
     num *= i;
   }
   return num;
@@ -33,5 +33,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
